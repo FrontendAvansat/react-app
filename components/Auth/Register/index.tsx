b0dfd79d02709b43d8c9b85624384812dfb8d245
Mari Barbu <barbu.mary54@yahoo.com>
2022-01-19 18:10:12 +0200

    [Unstable]

--- components/Auth/Register/index.tsx
+++ components/Auth/Register/index.tsx
@@ -1,52 +1,62 @@
-import { Input } from "@chakra-ui/core";
+import { Input, Flex } from "@chakra-ui/core";
 import NameWrap from "../../Common/NameWrap";
+import { useRegister } from "./useRegister";
 
 export const RegisterComponent = () => {
   return console.log;
-  // return (
-  //   <Flex justify={"center"} flex={1} align={"center"}>
-  //     <Flex
-  //       bg="grey.100"
-  //       color="white"
-  //       h="400px"
-  //       p="32px"
-  //       direction={"column"}
-  //       borderRadius={"10px"}
-  //       boxShadow={"5px 5px 20px black"}
-  //     >
-  //       <Flex justify={"center"} fontFamily={"primary"} fontSize={"h2"}>
-  //         Login
-  //       </Flex>
-  //       <NameWrap title="Name">
-  //         <Input
-  //           value={data?.email}
-  //           w="256px"
-  //           onChange={(e) => {
-  //             setData({ ...data, email: e.target.value });
-  //           }}
-  //         ></Input>
-  //       </NameWrap>
-  //       <NameWrap title="Email">
-  //         <Input
-  //           value={data?.email}
-  //           w="256px"
-  //           onChange={(e) => {
-  //             setData({ ...data, email: e.target.value });
-  //           }}
-  //         ></Input>
-  //       </NameWrap>
-  //       <NameWrap title="Password">
-  //         <Input
-  //           value={data?.password}
-  //           w="256px"
-  //           onChange={(e) => {
-  //             setData({ ...data, password: e.target.value });
-  //           }}
-  //         ></Input>
-  //       </NameWrap>
-  //       <CustomButton onClick={sendData} alignSelf={"center"}>
-  //         Login
-  //       </CustomButton>
-  //     </Flex>
-  //   </Flex>
+  return (
+    <Flex justify={"center"} flex={1} align={"center"}>
+      <Flex
+        bg="grey.100"
+        color="white"
+        h="400px"
+        p="32px"
+        direction={"column"}
+        borderRadius={"10px"}
+        boxShadow={"5px 5px 20px black"}
+      >
+        <Flex justify={"center"} fontFamily={"primary"} fontSize={"h2"}>
+          Login
+        </Flex>
+        <NameWrap title="First Name">
+          <Input
+            value={data?.firstName}
+            w="256px"
+            onChange={(e) => {
+              setData({ ...data, firstName: e.target.value });
+            }}
+          ></Input>
+        </NameWrap>
+        <NameWrap title="Last Name">
+          <Input
+            value={data?.lastName}
+            w="256px"
+            onChange={(e) => {
+              setData({ ...data, lastName: e.target.value });
+            }}
+          ></Input>
+        </NameWrap>
+        <NameWrap title="Email">
+          <Input
+            value={data?.email}
+            w="256px"
+            onChange={(e) => {
+              setData({ ...data, email: e.target.value });
+            }}
+          ></Input>
+        </NameWrap>
+        <NameWrap title="Password">
+          <Input
+            value={data?.password}
+            w="256px"
+            onChange={(e) => {
+              setData({ ...data, password: e.target.value });
+            }}
+          ></Input>
+        </NameWrap>
+        <CustomButton onClick={sendData} alignSelf={"center"}>
+          Login
+        </CustomButton>
+      </Flex>
+    </Flex>
 };
--- components/Auth/Register/useRegister.ts
+++ components/Auth/Register/useRegister.ts
@@ -3,18 +3,20 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../../../pages/_app";
 import { apiClient, authorisation } from "../../apiClient";
 
-export interface LoginData {
+export interface RegisterData {
+  firstName: string;
+  lastName: string;
   email: string;
   password: string;
 }
-export const useLogin = () => {
-  const [data, setData] = useState<LoginData>();
+export const useRegister = () => {
+  const [data, setData] = useState<RegisterData>();
   const { setData: setUser } = useContext(AuthContext);
   const { push } = useRouter();
   const sendData = async () => {
     try {
       const res = await apiClient.post(
-        "api/auth-service/account/login",
+        "api/auth-service/account/register",
         data,
         authorisation("This is the accesToken") //aici o sa am setUser(res.data)
       );
